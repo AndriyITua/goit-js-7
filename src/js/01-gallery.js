b985a4ef75511fb95d386b7ccb44c2187b0cbdfa
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2021-12-22 21:33:36 +0200

    Task-1.2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galletyContainer = document.querySelector('.gallery');
+const galleryCardMarkup = createImagesCardsMarkup(galleryItems);
+
+galletyContainer.insertAdjacentHTML('beforeend', galleryCardMarkup);
+
+galletyContainer.addEventListener('click', ongalletyContainerClick);
+
+function createImagesCardsMarkup (galleryItems) { 
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+             />
+            </a>
+            </div>
+            `
+        }).join('');
+}
+
+function ongalletyContainerClick(e) {
+
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+    console.log(e.target);
+}
+
 console.log(galleryItems);
